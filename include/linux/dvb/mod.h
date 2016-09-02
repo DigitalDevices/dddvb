7a8d3f80c4a4508b2fbf81bb1f632950dab9c500
Ralph Metzler <rjkm@metzlerbros.de>
2016-09-02 17:35:24 +0200

    add modulator properties

--- include/linux/dvb/mod.h
+++ include/linux/dvb/mod.h
@@ -19,4 +19,15 @@ struct dvb_mod_channel_params {
 #define DVB_MOD_SET              _IOW('o', 208, struct dvb_mod_params)
 #define DVB_MOD_CHANNEL_SET      _IOW('o', 209, struct dvb_mod_channel_params)
 
+#define MOD_UNDEFINED		0
+#define MOD_START		1
+#define MOD_STOP		2
+#define MOD_FREQUENCY		3
+#define MOD_MODULATION		4   
+#define MOD_SYMBOL_RATE		5   /* Hz */
+#define MOD_ATTENUATOR          32  
+#define MOD_INPUT_BITRATE       33  /* Hz */
+#define MOD_PCR_MODE            34  /* 1=pcr correction enabled */
+
+
 #endif /*_UAPI_DVBMOD_H_*/
