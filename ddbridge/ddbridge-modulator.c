8b9a107e7faa91dcb2103f1cb560cf5880d7e8fa
Marcin Kaluza <mk@dvb-s>
2017-10-19 14:33:58 +0200

    New API: fix setting MODULATOR_INPUT_BITRATE

--- ddbridge/ddbridge-modulator.c
+++ ddbridge/ddbridge-modulator.c
@@ -1541,7 +1541,7 @@ static int mod_prop_proc(struct ddb_mod *mod, struct dtv_property *tvp)
 		return mod_set_attenuator(mod->port->dev, tvp->u.data);
 
 	case MODULATOR_INPUT_BITRATE:
-		return mod_set_ibitrate(mod, tvp->u.data64);
+		return mod_set_ibitrate(mod, ((u64)tvp->u.data) << 32);
 
 	case MODULATOR_GAIN:
 		if (mod->port->dev->link[0].info->version == 2)
