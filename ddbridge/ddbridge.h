033432cf2fa9a41f817a719592d2da30e09234a1
Ralph Metzler <rjkm@metzlerbros.de>
2018-03-14 15:11:52 +0100

    increase version number

--- ddbridge/ddbridge.h
+++ ddbridge/ddbridge.h
@@ -516,7 +516,7 @@ struct DDMOD_FLASH {
 
 int ddbridge_flashread(struct ddb *dev, u32 link, u8 *buf, u32 addr, u32 len);
 
-#define DDBRIDGE_VERSION "0.9.32"
+#define DDBRIDGE_VERSION "0.9.33"
 
 /* linked function prototypes */
 
