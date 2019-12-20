e9f2f5788c08bb72f6b6a4f3662e1885efc92c93
none <git>
2019-12-20 22:47:08 +0100

    new release

--- ddbridge/ddbridge.h
+++ ddbridge/ddbridge.h
@@ -525,7 +525,7 @@ struct DDMOD_FLASH {
 
 int ddbridge_flashread(struct ddb *dev, u32 link, u8 *buf, u32 addr, u32 len);
 
-#define DDBRIDGE_VERSION "0.9.37"
+#define DDBRIDGE_VERSION "0.9.38"
 
 /* linked function prototypes */
 
