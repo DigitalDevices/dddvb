0a5fb7d6b9b2c91c09b248ca5345622ce7922fdb
rjkm <none>
2021-08-07 22:35:22 +0200

    add debugging output for actually tuned frequency

--- lib/src/dvb_quality.c
+++ lib/src/dvb_quality.c
@@ -329,7 +329,7 @@ static void calc_lq(struct dddvb_fe *fe)
 {
 	struct dtv_fe_stats st;
 	int64_t str, snr;
-	uint32_t mod, fec, ber_num, ber_den, trans, pilot = 0, quality = 0;
+	uint32_t mod, fec, ber_num, ber_den, trans, pilot = 0, quality = 0, freq;
 	
 	get_property(fe->fd, DTV_TRANSMISSION_MODE, &fe->pls_code);
 	dbgprintf(DEBUG_DVB, "fe%d: pls=0x%02x\n", fe->nr, fe->pls_code);
@@ -364,9 +364,13 @@ static void calc_lq(struct dddvb_fe *fe)
 	dbgprintf(DEBUG_DVB, "fe%d: fec=%u mod=%u\n", fe->nr, fec, mod);
 	switch (fe->n_param.param[PARAM_MSYS]) {
 	case SYS_DVBS:
+		get_property(fe->fd, DTV_FREQUENCY, &freq);
+		dbgprintf(DEBUG_DVB, "fe%d: actual frequency=%u\n", fe->nr, freq);
 		quality = dvbsq(snr, fec, ber_num, ber_den);
 		break;
 	case SYS_DVBS2:
+		get_property(fe->fd, DTV_FREQUENCY, &freq);
+		dbgprintf(DEBUG_DVB, "fe%d: actual frequency=%u\n", fe->nr, freq);
 		quality = dvbs2q(snr, fec, mod, ber_num, ber_den);
 		break;
 	case SYS_DVBC_ANNEX_A:
