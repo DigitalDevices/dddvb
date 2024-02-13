525472b9f1e00edcc38193f756bf8e2970c65c18
internal <>
2024-02-13 12:54:51 +0100

    0.9.39 release

--- ddbridge/ddbridge.h
+++ ddbridge/ddbridge.h
@@ -537,7 +537,7 @@ struct DDMOD_FLASH {
 
 int ddbridge_flashread(struct ddb *dev, u32 link, u8 *buf, u32 addr, u32 len);
 
-#define DDBRIDGE_VERSION "0.9.38"
+#define DDBRIDGE_VERSION "0.9.39"
 
 /* linked function prototypes */
 
